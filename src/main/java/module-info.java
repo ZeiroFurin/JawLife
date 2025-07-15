d88180bfc4527aceefb0ef516285a3aa20a0ef1b
ZeiroFurin <gerryrivanda167@gmail.com>
2025-07-15 21:27:10 +0700

    Dani beres

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module JawLife {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.media;
+    opens pinal to javafx.fxml; 
+    exports pinal;
+    
     requires org.controlsfx.controls;
     requires java.desktop;
 
